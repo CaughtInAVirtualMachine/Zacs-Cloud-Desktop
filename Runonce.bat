5c204da6d654e27c7cb39f9c20173b00d608952b
thetechlink <zac@garvelink.net>
2023-04-29 01:26:01 -0400

    Update Runonce.bat

--- Runonce.bat
+++ Runonce.bat
@@ -2,5 +2,5 @@ Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
 scoop install git 
 scoop bucket add extras
-scoop install chromium,notepadplusplus,gimp,rustdesk,mremoteng,windows-terminal
+scoop install chromium,notepadplusplus,gimp,rustdesk,mremoteng,windows-terminal,python
 reg import "C:\Users\zac\scoop\apps\notepadplusplus\current\install-context.reg"
