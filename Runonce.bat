cc972296c1f83eb6bfd38927eee477d659419bbe
CaughtInAVirtualMachine <zac@garvelink.net>
2024-12-07 14:01:38 -0800

    Update Runonce.bat

--- Runonce.bat
+++ Runonce.bat
@@ -2,5 +2,6 @@ Set-ExecutionPolicy RemoteSigned -Scope CurrentUser
 irm get.scoop.sh | iex
 scoop install git 
 scoop bucket add extras
-scoop install chromium,notepadplusplus,gimp,rustdesk,mremoteng,windows-terminal,python
+scoop install chromium,notepadplusplus,rustdesk,mremoteng,python,tailscale,telegram
 reg import "C:\Users\zac\scoop\apps\notepadplusplus\current\install-context.reg"
+https://getupnote.com/#get-upnote
